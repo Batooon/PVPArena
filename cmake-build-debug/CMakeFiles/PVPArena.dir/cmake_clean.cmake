c225bff1070fbe743fa1241e2e49e49fc946e0e0
Anton <rozum.ant@gmail.com>
2021-11-25 11:18:24 +0200

    project initialized

---
+++ cmake-build-debug/CMakeFiles/PVPArena.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PVPArena.dir/main.cpp.obj"
+  "PVPArena.exe"
+  "PVPArena.exe.manifest"
+  "PVPArena.pdb"
+  "libPVPArena.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PVPArena.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+
+	void run();
+
+	void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
+private:
+	void processEvents();
+
+	void update(sf::Time deltaTime);
+
+	void render();
+
+	sf::RenderWindow window;
+	sf::CircleShape player;
+
+	bool movingUp = false, movingDown = false, movingLeft = false, movingRight = false;
+};
+
+Game::Game():window(sf::VideoMode(640, 480), "SFML"), player()
+{
+	player.setRadius(40.f);
+	player.setPosition(100.f, 100.f);
+	player.setFillColor(sf::Color::Cyan);
+}
+
+void Game::run()
+{
+	while(window.isOpen())
+	{
+		processEvents();
+		render();
+		update();
+	}
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+	if(key == sf::Keyboard::W)
+		movingUp = isPressed;
+	else if(key == sf::Keyboard::S)
+		movingDown = isPressed;
+	else if(key == sf::Keyboard::A)
+		movingLeft = isPressed;
+	else if(key == sf::Keyboard::D)
+		movingRight = isPressed;
+}
+
+void Game::processEvents()
+{
+	sf::Event event;
+	while(window.pollEvent(event))
+	{
+		switch(event.type)
+		{
+			case sf::Event::KeyPressed:
+				handlePlayerInput(event.key.code, true);
+				break;
+			case sf::Event::KeyReleased:
+				handlePlayerInput(event.key.code, false);
+				break;
+			case sf::Event::Closed:
+				window.close();
+				break;
+		}
+	}
+}
+
+void Game::update(sf::Time deltaTime)
+{
+	sf::Vector2f movement(0.f, 0.f);
+	if(movingUp)
+		movement.y -= 1.f;
+	if(movingDown)
+		movement.y += 1.f;
+	if(movingLeft)
+		movement.x -= 1.f;
+	if(movingRight)
+		movement.x += 1.f;
+	player.move(movement);
+}
+
+void Game::render()
+{
+	window.clear();
+	window.draw(player);
+	window.display();
+}
+
+int main()
+{
+	Game game;
+	game.run();
+
+	return 0;
+}
