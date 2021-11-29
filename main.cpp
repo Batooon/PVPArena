cfd1cf62525603416291c658330c64caa75ba5a1
Anton <rozum.ant@gmail.com>
2021-11-30 01:29:05 +0200

    removed Normalize function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <cmath>
 #include <functional>
 #include "ResourceHolder.h"
 #include "SceneNode.h"
@@ -9,15 +8,6 @@
 #include "Soldier.h"
 #include "World.h"
 #include "Game.h"
-
-template<class VectorT>
-sf::Vector2f Normalize(VectorT vector)
-{
-	float length = sqrt((vector.x * vector.x) + (vector.y * vector.y));
-	if(length != 0)
-		return sf::Vector2f(vector.x / length, vector.y / length);
-	return (sf::Vector2f)vector;
-}
 
 const sf::Time Game::TimePerFrame = sf::seconds(1.f/120.f);
 
