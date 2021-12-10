ee314273888f43dc84e7f3ef8f02f1eefd544bdc
Anton <rozum.ant@gmail.com>
2021-12-10 15:56:54 +0200

    added TextNode

---
+++ TextNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anton on 10/12/2021.
+//
+
+#include "TextNode.h"
---
+++ TextNode.h
@@ -0,0 +1,15 @@
+//
+// Created by Anton on 10/12/2021.
+//
+
+#ifndef PVPARENA_TEXTNODE_H
+#define PVPARENA_TEXTNODE_H
+
+#include "SceneNode.h"
+
+class TextNode : public SceneNode
+{
+};
+
+
+#endif //PVPARENA_TEXTNODE_H
