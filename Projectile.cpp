f68ce1ccd43b4cbe3d525a673c06e6c90654deae
Anton <rozum.ant@gmail.com>
2021-12-18 21:14:01 +0200

    projectile carcass implemented

--- Projectile.cpp
+++ Projectile.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "Projectile.h"
+
+std::vector<ProjectileData> projectileData = initializeProjectileData();
+
+Projectile::Projectile(Projectile::Type type, const TextureHolder &textureHolder)
+: Entity(1)
+, type(type)
+, sprite(textureHolder.get(projectileData[type].Texture))
+{
+}
+
+unsigned int Projectile::getCategory() const
+{
+	switch(type)
+	{
+		case Type::Bullet:
+			return Category::Projectile;
+		case Type::EnemyBullet:
+			return Category::EnemyProjectile;
+	}
+}
+
+sf::FloatRect Projectile::getBounds() const
+{
+	return getWorldTransform().transformRect(sprite.getGlobalBounds());
+}
+
+float Projectile::getSpeed() const
+{
+	return projectileData[type].Speed;
+}
+
+int Projectile::getDamage() const
+{
+	return projectileData[type].Damage;
+}
+
+void Projectile::updateCurrent(sf::Time deltaTime, CommandQueue &commands)
+{
+	Entity::updateCurrent(deltaTime, commands);
+}
+
+void Projectile::drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const
+{
+	target.draw(sprite, states);
+}
--- Projectile.h
+++ Projectile.h
@@ -6,10 +6,29 @@
 #define PVPARENA_PROJECTILE_H
 
 #include "Entity.h"
+#include "Data.h"
 
 class Projectile : public Entity
 {
-	
+public:
+	enum Type
+	{
+		Bullet,
+		EnemyBullet,
+		Count
+	};
+
+	Projectile(Type type, const TextureHolder& textureHolder);
+	unsigned int getCategory() const override;
+	sf::FloatRect getBounds() const override;
+	float getSpeed() const;
+	int getDamage() const;
+
+private:
+	void updateCurrent(sf::Time deltaTime, CommandQueue& commands) override;
+	void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const override;
+	Type type;
+	sf::Sprite sprite;
 };
 
 
